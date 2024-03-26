16001da3c3d32608d585638f2bb435a01851befe
Kotov <dkotov04@gmail.com>
2024-03-26 10:18:52 +0300

    second commit

--- precode_test.go
+++ precode_test.go
@@ -3,51 +3,12 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
-	"strconv"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-var cafeList = map[string][]string{
-	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
-}
-
-func mainHandle(w http.ResponseWriter, req *http.Request) {
-	countStr := req.URL.Query().Get("count")
-	if countStr == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("count missing"))
-		return
-	}
-
-	count, err := strconv.Atoi(countStr)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong count value"))
-		return
-	}
-
-	city := req.URL.Query().Get("city")
-
-	cafe, ok := cafeList[city]
-	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("wrong city value"))
-		return
-	}
-
-	if count > len(cafe) {
-		count = len(cafe)
-	}
-
-	answer := strings.Join(cafe[:count], ",")
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(answer))
-}
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	//totalCount := 4
@@ -57,13 +18,15 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 	// здесь нужно добавить необходимые проверки
-	expectedCafes := []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"}
-	returnCafes := strings.Split(responseRecorder.Body.String(), ",")
-	// len(expectedCafes) вместо totalCount
-	require.Len(t, returnCafes, len(expectedCafes)) // Проверяем, что количество возвращенных кафе соответствует ожидаемому
-	for _, cafe := range expectedCafes {            // Проверяем, что все доступные кафе присутствуют в возвращенном списке
-		assert.Contains(t, returnCafes, cafe)
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	var expectedCafes []string
+	for _, cafe := range cafeList["moscow"] {
+		expectedCafes = append(expectedCafes, cafe)
 	}
+	returnCafes := strings.Split(responseRecorder.Body.String(), ",")
+	require.Len(t, returnCafes, len(expectedCafes))
+	assert.ElementsMatch(t, expectedCafes, returnCafes)
+	// len(expectedCafes) вместо totalCount
 }
 func TestMainHandlerWhenOk(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=3&city=moscow", nil)
